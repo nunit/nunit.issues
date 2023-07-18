04c056011010e332029c0b713eabfb42d0483701
Terje Sandstrom (AAD) <terje@hermit.no>
2023-07-18 13:00:45 +0200

    Updated 4406 and 4413

---
+++ Issue4413/AssumeTests.cs
@@ -0,0 +1,11 @@
+﻿namespace Issue4413;
+
+public class AssumeTests
+{
+    [TestCase(41, 43)]
+    [TestCase(42,43)]
+    public void CheckThatAssumeWorks(int x,int y)
+    {
+        Assume.That(x,Is.EqualTo(42));
+    }
+}

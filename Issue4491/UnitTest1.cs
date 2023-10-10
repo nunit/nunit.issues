04e7ac88641c6f0ed13ca2d6c7d19cfdb239eb19
Terje Sandstrom <terje@hermit.no>
2023-10-10 22:39:32 +0200

    Added a test to compare, Test3

--- Issue4491/UnitTest1.cs
+++ Issue4491/UnitTest1.cs
@@ -2,17 +2,24 @@ namespace Issue4491;
 
 public class Tests
 {
-    [Test]
-    [Repeat(30)]
-    public void Test1()
-    {
-        bool x = true;
-        Assert.That(x, Is.False);
-    }
+    //[Test]
+    //[Repeat(30)]
+    //public void Test1()
+    //{
+    //    bool x = true;
+    //    Assert.That(x, Is.False);
+    //}
 
     [Test]
     [Repeat(30)]
     public void Test2()
+    {
+        bool x = true;
+        Assert.That(x, Is.True);
+    }
+
+    [Test]
+    public void Test3()
     {
         bool x = true;
         Assert.That(x, Is.True);

d401fce3836205b8a3314427bbc8608b6582bf15
Terje Sandstrom <terje@hermit.no>
2023-11-27 19:12:43 +0100

    Added 4564

---
+++ Issue4564/NUnitRunnerWindows/TestFixture.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace NUnitRunnerWindows
+{
+    [TestFixture]
+    public class TestFixture
+    {
+        [Test]
+        public void SomeTest()
+        {
+        }
+    }
+}

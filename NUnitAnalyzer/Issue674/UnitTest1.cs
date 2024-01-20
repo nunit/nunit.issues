57f9966d4dd7e4ae0ef6fc3d2dd413ced83927f6
Terje Sandstrom <terje@hermit.no>
2024-01-20 14:08:54 +0100

    Added analyzer issue 674

---
+++ NUnitAnalyzer/Issue674/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Crashing;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.True(true);
+        Assert.False(false);
+    }
+}

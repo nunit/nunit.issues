f91afef49491bf4f09032b9a415daa097642fb3f
Terje Sandstrom <terje@hermit.no>
2024-02-29 12:13:00 +0100

    Issue4588

--- Issue4588/UnitTest1.cs
+++ Issue4588/UnitTest1.cs
@@ -2,11 +2,38 @@ namespace ValueTask;
 
 public class Tests
 {
-    public static ValueTask<string> Method() => new("");
+    private static ValueTask<string> Method() => new("");
 
+    private static Task<string> MethodT() => new(()=>"");
+
+
+    /// <summary>
+    /// Doesnt compile
+    /// </summary>
+    /// <returns></returns>
+    //[Test]
+    //public async Task Test0()
+    //{
+    //    await Assert.ThatAsync( () => Method(), Is.EqualTo(""));
+    //}
+
+
+
+    /// <summary>   
+    /// Workaround with adding async/await
+    /// </summary>
     [Test]
     public async Task Test1()
     {
-        await Assert.ThatAsync(()=>Method(),Is.EqualTo(""));
+        var result = await Method();
+        await Assert.ThatAsync(async () => await Method(), Is.EqualTo(""));
+    }
+
+    [Test]
+    public async Task Test2()
+    {
+        var result = await MethodT().ConfigureAwait(true);
+        Assert.That(result, Is.EqualTo(""));
+        //await Assert.ThatAsync(MethodT, Is.EqualTo(""));
     }
 }

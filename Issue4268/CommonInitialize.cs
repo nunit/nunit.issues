ca28acfb05557b1964a54a7cb970c18061ca5393
Terje Sandstrom <terje@hermit.no>
2024-03-01 20:52:11 +0100

    added Issue4268

---
+++ Issue4268/CommonInitialize.cs
@@ -0,0 +1,18 @@
+
+namespace Issue4268;
+
+[SetUpFixture]
+public class CommonInitialize
+{
+    [OneTimeSetUp]
+    public void Setup()
+    {
+        
+    }
+
+    [OneTimeTearDown]
+    public void Teardown()
+    {
+        
+    }
+}
---
+++ Issue4268/SomeTests.cs
@@ -0,0 +1,12 @@
+﻿namespace Issue4268
+{
+    
+    internal class SomeTests
+    {
+        [Test]
+        public void TestIt()
+        {
+            Assert.Pass();
+        }
+    }
+}

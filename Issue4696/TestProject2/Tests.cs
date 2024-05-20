25df6279c59446e8c1ccb487c62990f47f4c5b4a
Terje Sandstrom <terje@hermit.no>
2024-05-20 14:24:32 +0200

    Added Issue4696, mod a few others

---
+++ Issue4696/TestProject2/Tests.cs
@@ -0,0 +1,23 @@
+﻿
+using NUnit.Framework;
+
+namespace Issue4696.TestProject2
+{
+    public static class TestUtility
+    {
+        public static void Run(string message)
+        {
+            Assert.Pass(message);
+        }
+    }
+
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Run()
+        {
+            TestUtility.Run(nameof(Tests));
+        }
+    }
+}

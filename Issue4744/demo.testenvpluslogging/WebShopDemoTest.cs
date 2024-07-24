8a56b7a7fbc0f60c76013fd0c4ca71084cd2b9aa
Stefan Vogt <stefan.sv.vogt@siemens-healthineers.com>
2024-07-24 14:55:50 +0200

    Updated to HookExtension based on events and more advanced example added.

---
+++ Issue4744/demo.testenvpluslogging/WebShopDemoTest.cs
@@ -0,0 +1,38 @@
+﻿using demo.protocol.helper;
+using NUnit.Framework;
+
+namespace demo.devenv;
+
+public class WebShopDemoTest
+{
+    [OneTimeSetUp]
+    public void PrepareProductCatalog()
+    {
+        TestLog.Log("[OneTimeSetUp] add some products to the selling catalog");
+    }
+
+    [SetUp]
+    public void PrepareShoppingCart()
+    {
+        TestLog.Log("[SetUp] Set delivery address and payment method for customer user");
+    }
+
+
+    [Test]
+    public void SomeTest()
+    {
+        TestLog.Log("[Test] some testing in the created environment");
+    }
+
+    [TearDown]
+    public void CleanUserRelatedDataInOtherApps()
+    {
+        TestLog.Log("[TearDown] clean up custom user related data in other sub systems like payment or delivery");
+    }
+
+    [OneTimeTearDown]
+    public void CleanProductCatalog()
+    {
+        TestLog.Log("[OneTimeTearDown] clean up product catalog");
+    }
+}

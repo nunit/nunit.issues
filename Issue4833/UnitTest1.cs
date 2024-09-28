28974987c44593fbbba37b813bd9f5b389dfffd0
Terje Sandstrom <terje@hermit.no>
2024-09-28 21:18:44 +0200

    Added worker info

--- Issue4833/UnitTest1.cs
+++ Issue4833/UnitTest1.cs
@@ -2,7 +2,7 @@
 
 using System.Threading;
 
-[assembly: Parallelizable(ParallelScope.Fixtures)]
+[assembly: Parallelizable(ParallelScope.None)]
 
 
 namespace Issue4833;
@@ -11,13 +11,14 @@ public class Fixture
     [Test, Apartment(ApartmentState.STA)]
     public void Test1()
     {
-        TestContext.WriteLine(Thread.CurrentThread.ManagedThreadId);
+        TestContext.WriteLine($"{Thread.CurrentThread.ManagedThreadId}, {TestContext.CurrentContext.WorkerId}");
+        
     }
 
     [Test, Apartment(ApartmentState.STA)]
     public void Test2()
     {
-        TestContext.WriteLine(Thread.CurrentThread.ManagedThreadId);
+        TestContext.WriteLine($"{Thread.CurrentThread.ManagedThreadId}, {TestContext.CurrentContext.WorkerId}");
     }
 }
 
@@ -26,12 +27,12 @@ public class Fixture2
     [Test, Apartment(ApartmentState.STA)]
     public void Test3()
     {
-        TestContext.WriteLine(Thread.CurrentThread.ManagedThreadId);
+        TestContext.WriteLine($"{Thread.CurrentThread.ManagedThreadId}, {TestContext.CurrentContext.WorkerId}");
     }
 
     [Test, Apartment(ApartmentState.STA)]
     public void Test4()
     {
-        TestContext.WriteLine(Thread.CurrentThread.ManagedThreadId);
+        TestContext.WriteLine($"{Thread.CurrentThread.ManagedThreadId}, {TestContext.CurrentContext.WorkerId}");
     }
 }

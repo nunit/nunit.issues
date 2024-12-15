08172ddd5d1fc29d79865f76ebd2bcdcc8362bf1
Terje Sandstrom <terje@hermit.no>
2024-12-15 13:29:16 +0100

    updated

--- Issue4898/checkredisnull/UnitTest2.cs
+++ Issue4898/checkredisnull/UnitTest2.cs
@@ -1,26 +1,27 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
 using StackExchange.Redis;
 
 namespace NUnitIssue4898;
 
 public class Tests
 {
-	[Test]
-	public void TestStruct()
-	{
-		var redisResult = RedisValue.Null;
+    [Test]
+    public void TestStruct()
+    {
+        var redisResult = RedisValue.Null;
 
-		Assert.That(redisResult, Is.EqualTo(RedisValue.Null));
-	}
+        Assert.That(redisResult, Is.EqualTo(RedisValue.Null));
+    }
 
-	[Test]
-	public void TestStringValues()
-	{
-		var context = new DefaultHttpContext();
-		var response = context.Response;
-		response.Headers["X-Test"] = "test";
+    [Test]
+    public void TestStringValues()
+    {
+        var context = new DefaultHttpContext();
+        var response = context.Response;
+        response.Headers["X-Test"] = "test";
+        StringValues headerInfo = response.Headers["X-Test"];
+        Assert.That(headerInfo, Is.EqualTo("test"));
 
-		Assert.That(response.Headers["X-Test"], Is.EqualTo("test"));
-
-	}
+    }
 }

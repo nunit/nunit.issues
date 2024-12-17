9e957adb3003a332df3664bbe14543ba04135451
stevenaw <sweerdenburg@gmail.com>
2024-12-16 20:07:19 -0500

    testcasesource cancellationtoken

---
+++ Issue4900/CancellationTokenFromTestCaseSource/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿namespace CancellationTokenFromTestCaseSource
+{
+    public class Tests
+    {
+        [TestCaseSource(nameof(DivideCases)), CancelAfter(5000)]
+        public void DivideTest(int n, int d, int q, CancellationToken cancellationToken)
+        {
+            Assert.That(n / d, Is.EqualTo(q));
+        }
+
+        public static object[] DivideCases =
+        {
+            new object[] { 12, 3, 4 },
+            new object[] { 12, 2, 6 },
+            new object[] { 12, 4, 3 }
+        };
+    }
+}

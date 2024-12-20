1265bd523ed01df646a9310d11d374f8aeeb48d1
Terje Sandstrom <terje@hermit.no>
2024-12-20 21:19:51 +0100

    Discussion4869

---
+++ Discussion4869/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿namespace Discussion4869;
+
+public class MyIntegrationTest
+{
+    [Test,Order(1)]
+    public void Step1()
+    {
+        TestContext.Out.WriteLine(nameof(MyIntegrationTest)+"."+nameof(Step1));
+        Assert.Pass();
+    }
+
+    [Test, Order(2)]
+    public void Step2()
+    {
+        TestContext.Out.WriteLine(nameof(MyIntegrationTest) + "." + nameof(Step2));
+        Assert.Pass();
+    }
+
+    [Test, Order(3)]
+    public void Step3()
+    {
+        TestContext.Out.WriteLine(nameof(MyIntegrationTest) + "." + nameof(Step3));
+        Assert.Pass();
+    }
+}
---
+++ Discussion4869/UnitTest2.cs
@@ -0,0 +1,40 @@
+﻿namespace Discussion4869;
+
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+public class StepAttribute(int order) : TestAttribute
+{
+    public new void ApplyToTest(NUnit.Framework.Internal.Test test)
+    {
+        base.ApplyToTest(test);
+
+        // Apply the order to the test
+        test.Properties.Set(NUnit.Framework.Internal.PropertyNames.Order, order);
+    }
+}
+
+public class MyIntegrationTest2
+{
+    int i = 0;
+    [Step(1)]
+    public void Step1()
+    {
+        TestContext.Out.WriteLine($"{nameof(MyIntegrationTest2)}.{nameof(Step1)} {++i}");
+        Assert.Pass();
+    }
+
+    [Step(3)]
+    public void Step3()
+    {
+        TestContext.Out.WriteLine($"{nameof(MyIntegrationTest2)}.{nameof(Step3)} {++i}");
+        Assert.Pass();
+    }
+
+    [Step(2)]
+    public void Step2()
+    {
+        TestContext.Out.WriteLine($"{nameof(MyIntegrationTest2)}.{nameof(Step2)} {++i}");
+        Assert.Pass();
+    }
+}
+
+

a16989130e5bb4c9add24cec06ec78c789c8d4c4
Terje Sandstrom <terje@hermit.no>
2025-03-11 18:10:20 +1000

    added Issue4962

---
+++ Issue4962/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+namespace Issue4962;
+
+public class Tests
+{
+    [Test, CancelAfter(1000)]
+    public async Task SampleTest(CancellationToken cancellationToken)
+    {
+        var sw = new Stopwatch();
+        cancellationToken.Register(() =>
+        {
+            Console.Write($"Cancelled ");
+        });
+        
+        sw.Start();
+        try
+        {
+            await Task.Delay(15000, cancellationToken);
+        }
+        catch (Exception)
+        {}
+        finally
+        {}
+        Console.WriteLine($"after {sw.Elapsed}");
+        Assert.That(cancellationToken.IsCancellationRequested, Is.True);
+    }
+}

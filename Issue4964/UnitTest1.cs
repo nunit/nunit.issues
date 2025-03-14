8f65b52e5802393c4dbc1eec6dfad9534cf8709d
stevenaw <sweerdenburg@gmail.com>
2025-03-13 20:31:36 -0400

    Issue 4964

---
+++ Issue4964/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Issue4964;
+
+public class Tests
+{
+    [Test]
+    public void UsesProvidedIComparerWithNot()
+    {
+        var comparer = new ObjectComparer();
+        Assert.Multiple(() =>
+        {
+            //Assert.That(2 + 3, Is.Not.EqualTo(4).Using(comparer));
+            Assert.That(comparer.WasCalled, "Comparer was not called");
+        });
+    }
+}
+
+internal class ObjectComparer : IEqualityComparer<int>
+{
+    public bool WasCalled { get; private set; }
+
+    public bool Equals(int x, int y)
+    {
+        WasCalled = true;
+        return x == y;
+    }
+
+    public int GetHashCode([DisallowNull] int obj)
+    {
+        return obj.GetHashCode();
+    }
+}

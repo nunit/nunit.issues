8bdc51164fe84cbe92f2d2c2e5b74e25099ee157
Terje Sandstrom <terje@hermit.no>
2025-07-19 21:07:34 +0200

    added for nunut4.4.0

---
+++ Issue5011/currentversion/ReproClientTestWithCurrentNUnit.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace currentversion
+{
+
+
+
+    using Fhi.ClientCredentialsKeypairs;
+
+    namespace lastversion
+    {
+        /// <summary>
+        /// These tests reproduces the actual error.
+        /// Note that 
+        /// </summary>
+        public class ReproClientTest
+        {
+            [Test]
+            public void TestClientId()
+            {
+                var clientConfig = new ClientCredentialsConfiguration
+                {
+
+                };
+                string clientProductionId = "whatever";
+                Assert.That(clientProductionId, Is.Not.EqualTo(clientConfig));
+
+            }
+
+            [Test]
+            public void TestCLientIdAsList()
+            {
+                var clientConfigList = new List<ClientCredentialsConfiguration?>
+                {
+                    new ClientCredentialsConfiguration()
+                };
+                string clientProductionId = "whatever";
+                Assert.That(clientProductionId, Is.Not.EqualTo(clientConfigList.First()));
+
+            }
+        }
+    }
+
+}

e372a0ee65ff9c9f44ecd04100fe167ab8b87a00
Terje Sandstrom <terje@hermit.no>
2025-09-14 13:21:31 +0200

    Added Issue5031

---
+++ Issue5031/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿namespace Issue5031;
+
+public class Tests
+{
+    private int i = 1;
+    [Repeat(5)]
+    [Test]
+    public void Test1()
+    {
+        Log($"{i++} hi");
+    }
+
+    private void Log(string msg)
+    {
+        File.AppendAllText(@"..\..\..\log.log", msg + "\n");
+        Console.WriteLine(msg);
+    }
+}

a845cdf951c16032802636a0a78e3bd637218671
Terje Sandstrom <terje@hermit.no>
2025-09-19 13:25:34 +0200

    Issue5033 updated and adding 3148

--- Issue5033/UnitTest1.cs
+++ Issue5033/UnitTest1.cs
@@ -2,12 +2,21 @@
 
 public class Tests
 {
-    
+    [SetUp]
+    public void Setup()
+    { }
 
     [Test]
     public void Test1()
     {
         int x = 1;
         Assert.That(x, Is.EqualTo(42));
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        var stacktrace = TestContext.CurrentContext.Result.StackTrace;
+        Console.WriteLine(stacktrace);
     }
 }

441bfa4eec22142b84d9349379f3861601ca9465
Terje Sandstrom <terje@hermit.no>
2025-09-24 23:30:56 +0200

    Added Issue 5035

---
+++ Issue5035/testing/UnitTest1.cs
@@ -0,0 +1,163 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+using System.Runtime.Versioning;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging.Configuration;
+
+namespace testing;
+
+public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+{
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+
+        builder.ConfigureServices(services =>
+        {
+            services.AddLogging(loggingBuilder =>
+            {
+                loggingBuilder.ClearProviders();
+                loggingBuilder.AddNUnitLogger().SetMinimumLevel(LogLevel.Information);
+                loggingBuilder.AddConsole();
+                loggingBuilder.AddDebug();
+            });
+        });
+    }
+
+}
+
+#if ISTHISNEEDED
+public class NunitLogger<T> : ILogger<T>, IDisposable where T : notnull
+{
+    public void Dispose()
+    {
+    }
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception,
+        Func<TState, Exception, string> formatter)
+    {
+        TestContext.Out.WriteLine(state!.ToString());   // Seems duplicate of IlgNunitLogger l 65, and never called
+    }
+
+    public bool IsEnabled(LogLevel logLevel)
+    {
+        return true;
+    }
+
+    public IDisposable BeginScope<TState>(TState state) where TState : notnull
+    {
+        return this;
+    }
+}
+#endif
+
+public class IlgNunitLogger : ILogger, IDisposable
+{
+    public void Dispose()
+    {
+    }
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception,
+        Func<TState, Exception, string> formatter)
+    {
+        TestContext.Out.WriteLine(state!.ToString());
+    }
+
+    public bool IsEnabled(LogLevel logLevel)
+    {
+        return true;
+    }
+
+    public IDisposable BeginScope<TState>(TState state) where TState : notnull
+    {
+        return this;
+    }
+}
+
+
+
+[UnsupportedOSPlatform("browser")]
+[ProviderAlias("NUnitLogger")]
+public sealed class IlgNUnitLoggerProvider : ILoggerProvider
+{
+    public void Dispose()
+    {
+        Logger?.Dispose();
+    }
+
+    public ILogger CreateLogger(string categoryName)
+    {
+        if (Logger != null) return Logger;
+
+        Logger = new();
+        return Logger;
+    }
+
+    public static IlgNunitLogger? Logger { get; private set; }
+}
+
+public static class IlgNunitLoggerExtensions
+{
+    public static ILoggingBuilder AddNUnitLogger(this ILoggingBuilder builder)
+    {
+        builder.AddConfiguration();
+
+        builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<ILoggerProvider, IlgNUnitLoggerProvider>());
+
+        //LoggerProviderOptions.RegisterProviderOptions
+        //    <ColorConsoleLoggerConfiguration, ColorConsoleLoggerProvider>(builder.Services);
+
+        return builder;
+    }
+}
+
+
+
+
+
+
+public class Tests
+{
+    private CustomWebApplicationFactory<Program> factory;
+    private HttpClient? client;
+    private ILogger logger;
+
+    [OneTimeSetUp]
+    public void OneTimeSetup()
+    {
+        factory = new CustomWebApplicationFactory<Program>();
+    }
+
+    [SetUp]
+    public void Setup()
+    {
+        client = factory.CreateClient();
+        using var scope = factory.Services.CreateScope();
+        logger = scope.ServiceProvider.GetRequiredService<ILogger<Tests>>();
+    }
+
+    [TearDown]
+    public void Cleanup()
+    {
+        client?.Dispose();
+        client = null;
+    }
+
+    [OneTimeTearDown]
+    public void TearDown()
+    {
+        factory.Dispose();
+    }
+
+    [Test]
+    public async Task Test1()
+    {
+        logger.LogInformation("This is a test log message from the test.");
+        var result = await client.GetAsync("https://localhost:7010/weatherforecast");
+        Assert.That(result.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+    }
+}
---
+++ Issue5035/webapi/Program.cs
@@ -0,0 +1,51 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+// Get the logger from the DI Services collection, name ILogger;
+var logger = app.Services.GetRequiredService<ILogger<Program>>();
+
+string[] summaries =
+[
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+];
+
+app.MapGet("/weatherforecast", () =>
+{
+    logger.LogInformation("Inside weatherforecast method");
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+
+public partial class Program { } // Make the implicit Program class public so integration tests can reference it
+
+
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}

6bbb66fb858d9064c789d9a488c4e4d761218a52
Terje Sandstrom <terje@hermit.no>
2025-12-02 21:14:20 +0100

    Updated Issue1151

---
+++ Issue1151/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿namespace Issue1151;
+
+public class Tests
+{
+    /// <summary>
+    /// Repro for Issue 1151 : Include differences in output for Is.EquivalentTo
+    /// https://github.com/nunit/nunit/issues/1151
+    /// </summary>
+    [Test]
+    public void EquivalenceInformationTest()
+    {
+        var expected = Enumerable.Range('a', 12).Select(n => (char)n).ToList();
+        var actual = expected.Take(11);
+
+        var ex = Assert.Throws<AssertionException>(()=>Assert.That(actual, Is.EquivalentTo(expected)));
+        Assert.That(ex.Message, Does.Contain("Missing (1): < 'l' >"));
+    }
+}
---
+++ docs909/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿namespace docs909;
+
+public class Tests
+{
+   
+    
+[Test]
+public void CollectionContains_Examples()
+{
+    int[] intArray = {1, 2, 3};
+    string[] stringArray = ["a", "b", "c"];
+    
+    Assert.That(intArray, Has.Member(3));
+    Assert.That(stringArray, Has.Member("b"));
+    Assert.That(stringArray, Contains.Item("c"));
+    Assert.That(stringArray, Has.No.Member("x"));
+    Assert.That(intArray, Does.Contain(3));
+}
+
+}

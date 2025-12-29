f1753d6e3bab961290c34dd1af328898ba0da036
stevenaw <sweerdenburg@gmail.com>
2025-12-29 13:37:25 -0500

    Issue 5096

---
+++ Issue5096/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+
+namespace Issue5096;
+
+// Invoke as 'dotnet bin\Debug\net10.0\Issue5096.dll --timeout 5s'
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+        Thread.Sleep(10000);
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}

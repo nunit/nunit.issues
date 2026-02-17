b3b4fc5dcc6175347038ccd695587c4c405a4545
Terje Sandstrom <terje@hermit.no>
2026-02-17 21:21:19 +0100

    Added issue5126

---
+++ Issue5126/SetUpFixture.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Diagnostics;
+using System.Linq;
+
+using NUnit.Framework;
+using NUnit.Framework.Diagnostics;
+using NUnit.Framework.Internal;
+
+namespace Issue5126
+{
+    [SetUpFixture]
+    public class SetUpFixture
+    {
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            if (!Trace.Listeners.OfType<ProgressTraceListener>().Any())
+                Trace.Listeners.Add(new ProgressTraceListener());
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            Trace.WriteLine("TotalCount        = " + TestExecutionContext.CurrentContext.CurrentResult.TotalCount);
+            Trace.WriteLine("SkipCount         = " + TestExecutionContext.CurrentContext.CurrentResult.SkipCount);
+            Trace.WriteLine("InconclusiveCount = " + TestExecutionContext.CurrentContext.CurrentResult.InconclusiveCount);
+        }
+    }
+}
---
+++ Issue5126/TestFixture.cs
@@ -0,0 +1,21 @@
+﻿
+using NUnit.Framework;
+
+namespace Issue5126
+{
+    [TestFixture]
+    public class TestFixture
+    {
+        [Test, Explicit]
+        public void ExplicitTestMethod()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void NonExplicitTestMethod()
+        {
+            Assert.Pass();
+        }
+    }
+}

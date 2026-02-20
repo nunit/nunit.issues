d19f4082f847d1b7a9a1866ef76d5f3147f51ed8
Terje Sandstrom <terje@hermit.no>
2026-02-20 10:16:06 +0100

    Added Issue5129

---
+++ Issue5129/UnitTest1.cs
@@ -0,0 +1,74 @@
+﻿namespace Issue5129;
+
+public sealed class ArrayAsParameterTests
+{
+    private static IEnumerable<Array> Testcases
+    {
+        get
+        {
+            yield return new[] { 1, 2, 3 };
+        }
+    }
+
+    [TestCaseSource(nameof(Testcases))]
+    public void Foo(Array values)
+    {
+        Assert.That(values.Length, Is.EqualTo(3));
+    }
+}
+
+public sealed class IntArrayAsParameterTests
+{
+    private static IEnumerable<int[]> Testcases
+    {
+        get
+        {
+            yield return new[] { 1, 2, 3 };
+        }
+    }
+
+    [TestCaseSource(nameof(Testcases))]
+    public void Foo(int[] values)
+    {
+        Assert.That(values.Length, Is.EqualTo(3));
+    }
+}
+
+
+public sealed class ObjectAsParameterTests
+{
+    private static IEnumerable<object> Testcases
+    {
+        get
+        {
+            yield return new[] { 1, 2, 3 };
+        }
+    }
+
+    [TestCaseSource(nameof(Testcases))]
+    public void Foo(object values)
+    {
+
+        Assert.That(((Array)values).Length, Is.EqualTo(3));
+    }
+}
+
+
+
+public sealed class ListAsParameterTests
+{
+    private static IEnumerable<IList<int>> Testcases
+    {
+        get
+        {
+            yield return new List<int> { 1, 2, 3 };
+        }
+    }
+
+    [TestCaseSource(nameof(Testcases))]
+    public void Foo(IList<int> values)
+    {
+        Assert.That(values.Count, Is.EqualTo(3));
+    }
+}
+

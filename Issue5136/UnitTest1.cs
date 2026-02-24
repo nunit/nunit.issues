8572433c5fbb0fa48b3dfcac59e69782cd7a3506
Terje Sandstrom <terje@hermit.no>
2026-02-24 12:45:14 +0100

    Added repro Issue5136

---
+++ Issue5136/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿namespace Issue5136;
+
+public class Tests
+{
+    [TestCase(null)]
+    [TestCase([null])]
+    [TestCase((string?)null)]
+    public void Test1(params string?[] members)
+    {
+        Assert.That(members, Is.Not.Null);
+    }
+
+
+}
+
+public class Tests2
+{
+    [Test]
+    public void Verify()
+    {
+        var test = new Tests();
+        test.Test1((string?)null);
+    }
+}

272a1de13670c1616f4892de2ee10344c3398b38
Manfred Brands <manfred-brands@users.noreply.github.com>
2026-03-11 15:20:27 +0800

    Update Issue 5132
    Showcase the different behaviour of .NET Framework, .NET 8.0 and
    .NET 10.0 with the new SetUnhandledExceptionHandler.

--- Issue5132/UnitTest1.cs
+++ Issue5132/UnitTest1.cs
@@ -1,76 +1,146 @@
-﻿using NUnit.Framework.Internal;
+﻿// Copyright (c) Charlie Poole, Rob Prouse and Contributors. MIT License - see LICENSE.txt
 
-namespace Issue5132;
-
-public class Tests
+namespace NUnit.TestData
 {
-    [SetUp]
-    public void Setup()
+    [TestFixture]
+    public class UnhandledExceptionFixture
     {
-    }
+        private Task? _task;
+        private bool _exceptionCaught = true;
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
-        SimplifyAbort();
-    }
-    
-    [Test]
-    public void Test2()
-    {
-      Assert.Pass();
-    }
-
-    [Test]
-    public void Test3()
-    {
-        for (int i = 0; i < 3; i++)
-            SimplifyAbort();
-    }
-
-    [Test]
-    public void Test4()
-    {
-        for (int i = 0; i < 3; i++)
-            SimplifyAbortFix();
-    }
-
-    public static void SimplifyAbort()
+        [OneTimeSetUp]
+        public void OneTimeSetup()
         {
-            var thread = new Thread(() => 
-            {
-                Thread.Sleep(1000);
-            });
-            thread.Start();
-            thread.Join(500);
-            thread.Abort();
+            _exceptionCaught = false;
+
+#if NET10_0_OR_GREATER
+            System.Runtime.ExceptionServices.ExceptionHandling.SetUnhandledExceptionHandler(CaughtIt);
+#endif
+            AppDomain.CurrentDomain.UnhandledException += CaughtIt;
+            TaskScheduler.UnobservedTaskException += CaughtIt;
         }
 
-    public static void SimplifyAbortFix()
-    {
-        var thread = new Thread(() =>
+        [SetUp]
+        public void Setup()
+        {
+            _task = null;
+            _exceptionCaught = false;
+        }
+
+        [Test]
+        public void TestExceptionThrownInSpawnedThread()
+        {
+            var thread = new Thread(() =>
+            {
+                Thread.Sleep(50);
+                throw new InvalidOperationException("Raised from non-nunit Thread");
+            });
+            thread.Start();
+        }
+
+        [Test]
+        public void TestExceptionThrownInTask()
+        {
+            _task = Task.Run(async () =>
+            {
+                await Task.Delay(50);
+                throw new InvalidOperationException("Raised from non-nunit Task");
+            });
+        }
+
+        private void CaughtIt(object? sender, UnobservedTaskExceptionEventArgs e)
+        {
+            _exceptionCaught = true;
+        }
+
+        private void CaughtIt(object sender, UnhandledExceptionEventArgs e)
+        {
+            _exceptionCaught = true;
+        }
+
+#if NET10_0_OR_GREATER
+        private bool CaughtIt(Exception _)
+        {
+            _exceptionCaught = true;
+            return true;
+        }
+#endif
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            await Task.Delay(100);
+
+            if (_task is not null)
+            {
+                await Task.Yield();
+
+                // Ensure the task runs to completion using a backdoor,
+                // as standard awaiting it would cause the exception to be observed and
+                // thus not trigger the unobserved task exception behavior.
+                ((IAsyncResult)_task).AsyncWaitHandle.WaitOne();
+                Assume.That(_task.IsCompleted, Is.True);
+                _task = null;
+            }
+
+            for (int i = 0; i < 10 && !_exceptionCaught; i++)
+            {
+                // Force finalizers to run, which should cause unobserved task exceptions to be raised.
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+
+                await Task.Delay(10);
+            }
+
+            Assert.That(_exceptionCaught, Is.True, "Expected exception thrown was not caught");
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            AppDomain.CurrentDomain.UnhandledException -= CaughtIt;
+            TaskScheduler.UnobservedTaskException -= CaughtIt;
+        }
+
+#if NETFRAMEWORK
+        [Test]
+        public void TestThreadAbort()
+        {
+            LimitRunningTime(LongRunningOperations);
+        }
+
+        [Test]
+        public void TestThreadAbortCaughtAndReset()
+        {
+            LimitRunningTime(() => AbortSafe(LongRunningOperations));
+            // If we got here without an exception, the ThreadAbortException was successfully caught and reset.
+            _exceptionCaught = true;
+        }
+
+        private static void LimitRunningTime(Action action)
+        {
+            var thread = new Thread(_ => action());
+            thread.Start();
+            if (!thread.Join(500))
+            {
+                thread.Abort();
+                thread.Join(); // ensure it’s finished before returning
+            }
+        }
+
+        private static void AbortSafe(Action action)
         {
             try
             {
-                Thread.Sleep(1000);
+                action();
             }
             catch (ThreadAbortException)
             {
-                TestContext.Out.WriteLine("ThreadAbortException");
                 Thread.ResetAbort(); // optional: prevents rethrow at end of catch/finally
             }
-        });
+        }
 
-        thread.Start();
-        thread.Join(500);
-
-        if (thread.IsAlive)
-            thread.Abort();
-
-        thread.Join(); // ensure it’s finished before returning
+        private static void LongRunningOperations() => Thread.Sleep(1000);
+#endif
     }
-
 }
-
-

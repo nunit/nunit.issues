ef3f68bb9259007c8a1a7ba45828aa84f4dd12c3
stevenaw <sweerdenburg@gmail.com>
2026-03-17 06:35:16 -0400

    Issue5179 reproduction

---
+++ Issue5179/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿namespace Issue5179;
+public class Tests
+{
+    [TestCaseSource(nameof(MyTestData))]
+    public void PassDataFromNUnit_TestCaseSource(params object?[]? data)
+    {
+        // Assert
+        Assert.That(data, Is.Null);
+    }
+
+    [Test]
+    public void PassDataDirectly()
+    {
+        // Assert
+        PassDataFromNUnit_TestCaseSource(null);
+    }
+
+    public static object?[] MyTestData() => [null];
+}

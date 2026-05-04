5e7e1edf2286f4a752e59f39cc5d29375b1eeef2
Terje Sandstrom <terje@hermit.no>
2026-05-04 20:31:13 +0200

    Added repro for Issue5249

---
+++ Issue5249/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+
+[TestFixture]
+public class RetryTest
+{
+    [Retry(10, RetryExceptions = [typeof(Exception)])]
+    [Test]
+    public void RunsAll10ThenFails()
+    {
+        Console.WriteLine("try " + TestContext.CurrentContext.CurrentRepeatCount);
+        Assert.Fail();
+    }
+
+    [Retry(10, RetryExceptions = [typeof(Exception)])]
+    [Test]
+    public void InconclusiveAfterFirst()
+    {
+        Console.WriteLine("try " + TestContext.CurrentContext.CurrentRepeatCount);
+        throw new Exception();
+    }
+}

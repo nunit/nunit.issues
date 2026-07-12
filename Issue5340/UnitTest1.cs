97ab67802daeaf450c2cdc5632bff3c9b82cc7fe
Terje Sandstrom <terje@hermit.no>
2026-07-12 14:58:05 +0200

    Added/modified issue 5340, 5320, 2783 , 1194 and discussion 5124

---
+++ Issue5340/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿namespace Issue5340;
+
+public class Tests
+{
+    
+
+    [Test]
+    public void Test1()
+    {
+        var whatever = new Whatever
+        {
+            Capacity = 5
+        };
+        Assert.That(whatever, Has.Property(nameof(Whatever.Capacity)).EqualTo(5));
+    }
+
+    [Test]
+    public void Test2()
+    {
+        var whatever = new Whatever
+        {
+            Capacity = 5
+        };
+        Assert.That(whatever.Capacity, Is.EqualTo(5));
+    }
+
+    
+}
+
+public class Whatever
+{
+    public nint Capacity { get; set; }
+}

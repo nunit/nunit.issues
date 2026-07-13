1755f0fd5393ab734cef52aa5c2a89a708033538
stevenaw <sweerdenburg@gmail.com>
2026-07-13 09:01:56 -0400

    Add 3687 repro

---
+++ Issue3687/Program.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+Console.WriteLine("Hello, World!");
+
+new NUnitLite.AutoRun().Execute(args);
+
+[TestFixture]
+public class MyTests
+{
+    [Test]
+    public void Test1()
+    {
+        Assert.AreEqual(2, 1 + 1);
+    }
+    [Test]
+    public void Test2()
+    {
+        Assert.IsTrue(true);
+    }
+}

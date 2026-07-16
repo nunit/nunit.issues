1adfbb47b3d472d39be8efc6907c2fd1910ff004
stevenaw <sweerdenburg@gmail.com>
2026-07-16 16:57:54 -0400

    Split into projects to facilitate testing through multiple runners

--- Issue5347/Issue5347.Tests/Init.cs
+++ Issue5347/Issue5347.Tests/Init.cs
@@ -1,60 +1,6 @@
-﻿using NUnit.Framework;
-using NUnit.Framework.Interfaces;
+﻿using NUnit.Framework.Interfaces;
 using System.Reflection;
-
-Console.WriteLine("Hello, World!");
-
-new NUnitLite.AutoRun().Execute(args);
-
-[TestFixture]
-public class MyTests
-{
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
-    }
-
-    [Category("ExampleCategory")]
-    [Test]
-    public void Test2()
-    {
-        Assert.Pass();
-    }
-
-    [Property("Whatever", 42)]
-    [Category("ExampleCategoryTwo")]
-    [Test]
-    public void Test3()
-    {
-        Assert.Pass();
-    }
-}
-
-[TestFixture]
-public class MyNonTests
-{
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
-    }
-
-    [Category("ExampleCategory")]
-    [Test]
-    public void Test2()
-    {
-        Assert.Pass();
-    }
-
-    [Property("Whatever", 42)]
-    [Category("ExampleCategoryTwo")]
-    [Test]
-    public void Test3()
-    {
-        Assert.Pass();
-    }
-}
+using NUnit.Framework;
 
 [SetUpFixture]
 public class Init
---
+++ Issue5347/Issue5347.Tests/Tests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class MyTests
+{
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+
+    [Category("ExampleCategory")]
+    [Test]
+    public void Test2()
+    {
+        Assert.Pass();
+    }
+
+    [Property("Whatever", 42)]
+    [Category("ExampleCategoryTwo")]
+    [Test]
+    public void Test3()
+    {
+        Assert.Pass();
+    }
+}
+
+[TestFixture]
+public class MyNonTests
+{
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+
+    [Category("ExampleCategory")]
+    [Test]
+    public void Test2()
+    {
+        Assert.Pass();
+    }
+
+    [Property("Whatever", 42)]
+    [Category("ExampleCategoryTwo")]
+    [Test]
+    public void Test3()
+    {
+        Assert.Pass();
+    }
+}

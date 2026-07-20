bce25678dd7c9a982ef67900497b3f01fb55931a
Terje Sandstrom <terje@hermit.no>
2026-07-20 22:09:50 +0200

    fix

--- Issue2610/UnitTest1.cs
+++ Issue2610/UnitTest1.cs
@@ -3,39 +3,39 @@
 using System.Threading;
 using NUnit.Framework;
 
-    [TestFixture, SingleThreaded]
-    [Parallelizable(ParallelScope.Fixtures)] // or ParallelScope.All
-    internal class TestClass
+[TestFixture, SingleThreaded]
+[Parallelizable(ParallelScope.Fixtures)] // or ParallelScope.All
+internal class TestClass
+{
+    private Thread _setupThread;
+
+    [OneTimeSetUp]
+    public void OTSetup()
     {
-        private Thread _setupThread;
-
-        [OneTimeSetUp]
-        public void OTSetup()
-        {
-            _setupThread = Thread.CurrentThread;
-        }
-
-        [SetUp]
-        public void Setup()
-        {
-            Assert.That(Thread.CurrentThread, Is.EqualTo(_setupThread));
-        }
-
-        [Test]
-        public void Test()
-        {
-            Assert.That(Thread.CurrentThread, Is.EqualTo(_setupThread));
-        }
-
-        [TearDown]
-        public void Teardown()
-        {
-            Assert.That(Thread.CurrentThread, Is.EqualTo(_setupThread));
-        }
-
-        [OneTimeTearDown]
-        public void OTTeardown()
-        {
-            Assert.That(Thread.CurrentThread, Is.EqualTo(_setupThread)); // failure, not always
-        }
+        _setupThread = Thread.CurrentThread;
     }
+
+    [SetUp]
+    public void Setup()
+    {
+        Assert.That(Thread.CurrentThread, Is.EqualTo(_setupThread));
+    }
+
+    [Test]
+    public void Test()
+    {
+        Assert.That(Thread.CurrentThread, Is.EqualTo(_setupThread));
+    }
+
+    [TearDown]
+    public void Teardown()
+    {
+        Assert.That(Thread.CurrentThread, Is.EqualTo(_setupThread));
+    }
+
+    [OneTimeTearDown]
+    public void OTTeardown()
+    {
+        Assert.That(Thread.CurrentThread, Is.EqualTo(_setupThread)); // failure, not always
+    }
+}

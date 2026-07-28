965c35dfe75a94d7925f47ca0810651429da9f90
stevenaw <sweerdenburg@gmail.com>
2026-07-28 08:08:19 -0400

    Add static + dynamic examples for nunitlite prefiltering

--- Issue5347/Issue5347.Tests/Tests.cs
+++ Issue5347/Issue5347.Tests/Tests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using NUnit.Framework.Interfaces;
 
 [TestFixture]
 public class MyTests
@@ -48,4 +49,48 @@ public class MyNonTests
     {
         Assert.Pass();
     }
+}
+
+
+public class UnitTest1
+{
+    public class DataProvider<T> where T : TestData, new()
+    {
+        public static IEnumerable<TestCaseData> GetData()
+        {
+            string[] names = new string[] { "Matthew", "Kelly", "Shawn" };
+            char[] genders = new char[] { 'M', 'F', 'M' };
+            int[] ages = new int[] { 35, 34, 30 };
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                TestData testData = new TestData()
+                {
+                    Name = names[i],
+                    Gender = genders[i],
+                    Age = ages[i]
+                };
+
+                TestCaseData tcd = new TestCaseData(testData);
+                tcd.SetName("{m}("+ testData.Name + "-" + testData.Gender + "-" + testData.Age +")");
+
+                yield return tcd;
+            }
+        }
+    }
+
+
+    [Test]
+    [TestCaseSource(typeof(DataProvider<TestData>), "GetData")]
+    public void Persons(TestData test)
+    {
+        Assert.IsTrue(test.Name == "Matthew");
+    }
+}
+
+public class TestData
+{
+    public string Name { get; init; }
+    public char Gender { get; init; }
+    public int Age { get; init; }
 }

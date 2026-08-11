ef885111efdd73daa270819d1dd44f0e6e21f770
stevenaw <sweerdenburg@gmail.com>
2026-08-11 12:51:07 -0400

    AOT example

---
+++ Issue5358-NUnitLiteAOT/Program.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+new NUnitLite.AutoRun(typeof(CustomTests).Assembly).Execute(args);
+
+[TestFixture]
+public class CustomTests
+{
+    [Test]
+    public void Test1()
+    {
+        Assert.That(1, Is.EqualTo(1));
+    }
+}

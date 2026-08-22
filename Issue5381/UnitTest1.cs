eb725048ae73132858219e9434b83f050e64d7db
Terje Sandstrom <terje@hermit.no>
2026-08-22 12:43:23 +0200

    Added issue 5381

---
+++ Issue5381/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace Issue5381;
+
+[TestFixture(TestName = "abc..def")]
+public class Test9f62e7206ab64d01af310021ee2fad44
+{
+    [Test]
+    public async Task TestMethod()
+    {
+        Assert.Pass();
+    }
+}

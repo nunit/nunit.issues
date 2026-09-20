ff04b5948e38b8383a94dac107b91e9b24e7f148
Terje Sandstrom <terje@hermit.no>
2026-09-20 13:54:35 +0200

    Added Discussion5403 and Issue1488

---
+++ Discussion5403/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿namespace Discussion5403;
+
+public class Tests
+{
+    [Test]
+    public void Test1()
+    {
+        int x = 42;
+        Assert.That(x,Is.EqualTo(42));
+        Assert.Ignore("The last ignore");
+    }
+}
---
+++ Issue1488/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Repro;
+
+public class FilterRepro
+{
+    [TestCaseSource(nameof(Cases))]
+    public void EscapedOperator(string value) => Assert.That(value, Is.Not.Null);
+
+    private static IEnumerable<TestCaseData> Cases()
+    {
+        yield return new TestCaseData("a").SetName("{m}(Case 1: X = Y)");
+    }
+}

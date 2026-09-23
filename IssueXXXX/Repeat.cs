9019cf3fc632849dfe0062b25a2a1852f5e692ac
Mark Jordan <Mark.Jordan@red-gate.com>
2026-09-23 10:17:58 +0100

    works with basic setup

---
+++ IssueXXXX/Repeat.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace IssueXXXX;
+
+public class Repeat
+{
+    [Test, Repeat(20)]
+    public void TestMethod()
+    {
+        if (Random.Shared.NextDouble() > 0.5)
+        {
+            Assert.Fail("Random failure");
+        }
+    }
+}
